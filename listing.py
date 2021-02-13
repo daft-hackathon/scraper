77c9aae4e6f991efdd02968c1e3e448f97037720
Damian Anslik <damiananslik@outlook.com>
2021-02-13 19:38:32 +0000

    Added a price formatter method

--- listing.py
+++ listing.py
@@ -1,7 +1,11 @@
 class Listing:
     def __init__(self, address, price, property_type, num_bedrooms, num_bathrooms):
         self.address = address
-        self.price = price
+        self.price = self.format_price(price)
         self.property_type = property_type
         self.num_bedrooms = num_bedrooms
         self.num_bathrooms = num_bathrooms
+
+    def format_price(self, price):
+        new_price_string = "".join([char for char in price if char.isdigit()])
+        return int(new_price_string) if new_price_string else None    
